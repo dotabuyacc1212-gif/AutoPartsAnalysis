2d3554001bf0107c947eb80189e85f045df09e39
dotabuyacc1212-gif <dotabuyacc1212@gmail.com>
2026-01-16 16:38:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import plotly.graph_objects as go
 from pathlib import Path
 
 st.set_page_config(
-    page_title="Auto Parts Analysis",
+    page_title="Auto-Parts Analysis",
     page_icon="🚗",
     layout="wide"
 )
